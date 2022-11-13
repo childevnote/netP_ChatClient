e23dc9d420e21fe4ab31f209baa2867744933832
kdj <64907633+childevnote@users.noreply.github.com>
2022-11-14 00:18:13 +0900

    loginapp
    기본 창

---
+++ src/LoginApp.java
@@ -0,0 +1,18 @@
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class LoginApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFrame frame = new JFrame();
+		JPanel list = new JPanel();
+		
+		frame.setVisible(true);
+		frame.setSize(350, 550);
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
+	}
+
+}
