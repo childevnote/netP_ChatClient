e7954c3994ca41af6265007584321cb2114e005b
kyw <96057775+blueberrycoffee@users.noreply.github.com>
2022-11-14 19:31:35 +0900

    profiles(add)22.11.14
    프로필 사이드바 추가

--- src/ProfileList.java
+++ src/ProfileList.java
@@ -5,18 +5,55 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.JPopupMenu;
+import java.awt.Component;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class ProfileList {
 	public static final Color YELLOW = new Color(250, 225, 0);
 	public static final Color BROWN = new Color(82, 55, 56);
+	ImageIcon homeImg=new ImageIcon("src\\img\\home_FILL0.png");
+	ImageIcon chatImg=new ImageIcon("src\\img\\chat_bubble.png");
+	ImageIcon settingImg=new ImageIcon("src\\img\\settings_FILL0.png");
 	
 	public ProfileList() {
 		JFrame frame = new JFrame("카카오톡");
 		JPanel pnlBg = new JPanel();
 
-		pnlBg.setBackground(YELLOW);
+		pnlBg.setBackground(new Color(255, 255, 255)); //왼쪽 사이드바
 		pnlBg.setLayout(null);
-		frame.add(pnlBg);
+		frame.getContentPane().add(pnlBg);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(227, 228, 230));
+		panel.setForeground(new Color(255, 255, 255));
+		panel.setBounds(0, 0, 66, 611);
+		pnlBg.add(panel);
+		
+		JButton homeButton = new JButton(homeImg); //홈버튼
+		homeButton.setForeground(new Color(255, 255, 255));
+		homeButton.setBackground(new Color(227, 228, 230));
+		homeButton.setBounds(0, 0, 66, 68);
+		homeButton.setBorderPainted(false);
+		homeButton.setFocusPainted(false);
+		pnlBg.add(homeButton);
+		
+		JButton chatButton=new JButton(chatImg);//채팅방 버튼
+		chatButton.setForeground(new Color(255, 255, 255));
+		chatButton.setBackground(new Color(227, 228, 230));
+		chatButton.setBounds(0, 69, 66, 68);
+		chatButton.setBorderPainted(false);
+		chatButton.setFocusPainted(false);
+		pnlBg.add(chatButton);
+		
+		JButton settingButton=new JButton(settingImg);//설정 버튼
+		settingButton.setForeground(new Color(255, 255, 255));
+		settingButton.setBackground(new Color(227, 228, 230));
+		settingButton.setBounds(0, 129, 66, 68);
+		settingButton.setBorderPainted(false);
+		settingButton.setFocusPainted(false);
+		pnlBg.add(settingButton);
 		
 		frame.setVisible(true);
 		frame.setSize(450, 650);
