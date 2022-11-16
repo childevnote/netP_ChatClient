aaab0b7e67baa43978c396e217d68cc822d2a510
kdj <64907633+childevnote@users.noreply.github.com>
2022-11-16 17:56:55 +0900

    kdj
    kdj

--- src/LoginApp.java
+++ src/LoginApp.java
@@ -5,8 +5,12 @@ import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class LoginApp {
+	public static final int IDNUM = 10;
 	public static final Color YELLOW = new Color(250, 225, 0);
 	public static final Color BROWN = new Color(82, 55, 56);
+	public static final String[] idList = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j" };
+	String conn_id = null;
+	Boolean idcheck = false;
 
 	public LoginApp() {
 		JFrame frame = new JFrame("로그인");
@@ -16,20 +20,17 @@ public class LoginApp {
 		Image img_icon = toolkit.getImage("src\\img\\img_icon.png");
 
 		ImageIcon img;
-		
+
 		JTextField insert_id = new JTextField();
 		insert_id.setFont(new Font("맑은 고딕 Semilight", Font.ITALIC, 12));
 		insert_id.setText("아이디");
 		insert_id.setToolTipText("아이디");
-		JTextField insert_pw = new JTextField();
-		insert_pw.setFont(new Font("맑은 고딕 Semilight", Font.ITALIC, 12));
-		insert_pw.setText("비밀번호");
-		insert_pw.setToolTipText("비밀번호");
+
 		JButton loginbtn = new JButton("로그인");
 
 		img = new ImageIcon("src\\img\\icon.png");
 		lblImg = new JLabel(img);
-		
+
 		pnlBg.setBackground(YELLOW);
 		pnlBg.setLayout(null);
 		frame.getContentPane().add(pnlBg);
@@ -42,9 +43,6 @@ public class LoginApp {
 		insert_id.setBounds(125, 240, 200, 30);
 		pnlBg.add(insert_id);
 
-		insert_pw.setBounds(125, 270, 200, 30);
-		pnlBg.add(insert_pw);
-
 		loginbtn.setBounds(125, 310, 200, 40);
 		loginbtn.setBackground(BROWN);
 		loginbtn.setForeground(YELLOW);
@@ -52,8 +50,24 @@ public class LoginApp {
 
 		loginbtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				new ProfileList();
-				frame.setVisible(false);
+				for (int i = 0; i < IDNUM; i++) {
+					if (insert_id.getText().equals(idList[i])) {
+						idcheck = true;
+						conn_id = idList[i];
+						break;
+					}
+				}
+				if (idcheck) {
+					JOptionPane.showMessageDialog(null, conn_id + "님, 환영합니다.", "로그인 성공",
+							JOptionPane.INFORMATION_MESSAGE);
+					new ProfileList(conn_id);
+					frame.setVisible(false);
+				} else {
+					insert_id.setText("");
+					JOptionPane.showMessageDialog(null, "아이디가 존재하지 않습니다.", "로그인 오류", 
+							JOptionPane.WARNING_MESSAGE);
+				}
+
 			}
 		});
 
--- src/ProfileList.java
+++ src/ProfileList.java
@@ -21,52 +21,51 @@ public class ProfileList {
 
 	public static final Color YELLOW = new Color(250, 225, 0);
 	public static final Color BROWN = new Color(82, 55, 56);
-	
+
 	Toolkit toolkit = Toolkit.getDefaultToolkit();
 	Image img_icon = toolkit.getImage("src\\img\\img_icon.png");
-	
-	
+
 	ImageIcon homeImg = new ImageIcon("src\\img\\home_FILL0.png");
 	ImageIcon chatImg = new ImageIcon("src\\img\\chat_bubble.png");
 	ImageIcon settingImg = new ImageIcon("src\\img\\settings_FILL0.png");
 	ImageIcon logoutImg = new ImageIcon("src\\img\\logout.png");
-	ImageIcon userImg=new ImageIcon("src\\img\\account_circle.png");
-	
+	ImageIcon userImg = new ImageIcon("src\\img\\account_circle.png");
 
-	public ProfileList() {
+	public ProfileList(String conn_id) {
 		JFrame frame = new JFrame("카카오톡");
 		JPanel pnlBg = new JPanel();
 		JPanel pnlList = new JPanel();
 		frame.setIconImage(img_icon);
 
-		pnlBg.setBackground(new Color(255, 255, 255)); 
+		pnlBg.setBackground(new Color(255, 255, 255));
 		pnlBg.setLayout(null);
 		frame.getContentPane().add(pnlBg);
-		
-		JPanel sidePanel = new JPanel(); 
-		sidePanel.setBackground(new Color(227, 228, 230)); 
+
+		JPanel sidePanel = new JPanel();
+		sidePanel.setBackground(new Color(227, 228, 230));
 		sidePanel.setForeground(new Color(255, 255, 255));
 		sidePanel.setLayout(null);
 		sidePanel.setBounds(0, 0, 70, 650);
 		pnlBg.add(sidePanel);
+
 		
-		JButton homeButton = new JButton(homeImg); //프로필버튼
+		JButton homeButton = new JButton(homeImg); // 프로필버튼
 		homeButton.setForeground(new Color(255, 255, 255));
 		homeButton.setBackground(new Color(227, 228, 230));
 		homeButton.setBounds(0, 0, 70, 70);
 		homeButton.setBorderPainted(false);
 		homeButton.setFocusPainted(false);
 		sidePanel.add(homeButton);
-	
-		JButton chatButton=new JButton(chatImg);//채팅방버튼
+
+		JButton chatButton = new JButton(chatImg);// 채팅방버튼
 		chatButton.setForeground(new Color(255, 255, 255));
 		chatButton.setBackground(new Color(227, 228, 230));
 		chatButton.setBounds(0, 60, 70, 70);
 		chatButton.setBorderPainted(false);
 		chatButton.setFocusPainted(false);
 		sidePanel.add(chatButton);
-    
-		JButton settingButton=new JButton(settingImg); //설정 버튼
+
+		JButton settingButton = new JButton(settingImg); // 설정 버튼
 		settingButton.setForeground(new Color(255, 255, 255));
 		settingButton.setBackground(new Color(227, 228, 230));
 		settingButton.setBounds(0, 125, 70, 70);
@@ -74,68 +73,68 @@ public class ProfileList {
 		settingButton.setFocusPainted(false);
 		sidePanel.add(settingButton);
 
-		JButton logoutButton = new JButton(logoutImg);//로그아웃버튼
+		JButton logoutButton = new JButton(logoutImg);// 로그아웃버튼
 		logoutButton.setForeground(new Color(255, 255, 255));
 		logoutButton.setBackground(new Color(227, 228, 230));
 		logoutButton.setBounds(0, 530, 70, 70);
 		logoutButton.setBorderPainted(false);
 		logoutButton.setFocusPainted(false);
 		sidePanel.add(logoutButton);
-		
-		JPanel loginUser = new JPanel();//유저
+
+		JPanel loginUser = new JPanel();// 유저
 		loginUser.setBackground(new Color(255, 255, 255));
 		loginUser.setBounds(70, 0, 368, 80);
 		pnlBg.add(loginUser);
 		loginUser.setLayout(null);
-		
+
 		JButton loginUserButton = new JButton(userImg);
 		loginUserButton.setBounds(0, 0, 77, 80);
 		loginUserButton.setBackground(new Color(255, 255, 255));
 		loginUserButton.setBorderPainted(false);
 		loginUserButton.setFocusPainted(false);
 		loginUser.add(loginUserButton);
-		
-		JLabel lblNewLabel = new JLabel("Master");
+
+		JLabel lblNewLabel = new JLabel(conn_id);
 		lblNewLabel.setFont(new Font("맑은 고딕 Semilight", Font.BOLD, 15));
 		lblNewLabel.setBounds(89, 35, 160, 15);
 		loginUser.add(lblNewLabel);
 
-		JPanel loginUser01 = new JPanel(); //User1
+		JPanel loginUser01 = new JPanel(); // User1
 		loginUser01.setBackground(new Color(255, 255, 255));
 		loginUser01.setBounds(70, 80, 368, 80);
 		pnlBg.add(loginUser01);
 		loginUser01.setLayout(null);
-		
+
 		JButton loginUserButton01 = new JButton(userImg);
 		loginUserButton01.setBounds(0, 0, 77, 80);
 		loginUserButton01.setBackground(new Color(255, 255, 255));
 		loginUserButton01.setBorderPainted(false);
 		loginUserButton01.setFocusPainted(false);
 		loginUser01.add(loginUserButton01);
-		
+
 		JLabel lblNewLabel01 = new JLabel("User1");
 		lblNewLabel01.setFont(new Font("맑은 고딕 Semilight", Font.BOLD, 15));
 		lblNewLabel01.setBounds(89, 35, 160, 15);
 		loginUser01.add(lblNewLabel01);
-		
-		JPanel loginUser02 = new JPanel(); //User2
+
+		JPanel loginUser02 = new JPanel(); // User2
 		loginUser02.setBackground(new Color(255, 255, 255));
 		loginUser02.setBounds(70, 160, 368, 80);
 		pnlBg.add(loginUser02);
 		loginUser02.setLayout(null);
-		
+
 		JButton loginUserButton02 = new JButton(userImg);
 		loginUserButton02.setBounds(0, 0, 77, 80);
 		loginUserButton02.setBackground(new Color(255, 255, 255));
 		loginUserButton02.setBorderPainted(false);
 		loginUserButton02.setFocusPainted(false);
 		loginUser02.add(loginUserButton02);
-		
+
 		JLabel lblNewLabel02 = new JLabel("User2");
 		lblNewLabel02.setFont(new Font("맑은 고딕 Semilight", Font.BOLD, 15));
 		lblNewLabel02.setBounds(89, 35, 160, 15);
 		loginUser02.add(lblNewLabel02);
-		
+
 		frame.setVisible(true);
 		frame.setSize(450, 650);
 		frame.setLocationRelativeTo(null);
